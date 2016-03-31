8f1a591175c49b00f0df04eaec1d39921ea7833a
Brad Fincher <matrixv01@gmail.com>
2016-03-30 21:33:18 -0500

    Remove check out our github

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -21,9 +21,6 @@ var HomePage = React.createClass({
             Welcome to
             <span className="red">RNTR</span>!
           </div>
-          <div className="animated bounce">
-            <a target="_blank" href="http://bit.ly/1RCeXpx">visit our github!</a>
-          </div>
         </div>
       </div>
     );
